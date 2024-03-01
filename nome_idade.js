8f76ed02a2ab069b47993e4b326e007881c86d50
Dud4Mendes <129318829+Dud4Mendes@users.noreply.github.com>
2024-03-01 16:53:15 -0300

    Add files via upload

---
+++ nome_idade.js
@@ -0,0 +1,8 @@
+let nome = `duda`;
+let idade = 16;
+let altura = 175;
+let peso = 69;
+let inteiro = 2;
+
+console.log(`meu nome é ${nome} e minha idade é ${idade} e minha altura é ${altura} e meu peso é ${peso}`)
+console.log(`somando ${idade + inteiro}`)
---
+++ string.js
@@ -0,0 +1,6 @@
+let nome = 'geo';
+let endereco = "M.C.R";
+let cachorro = 'meg';
+console.log(`o nome da cachorra é ${cachorro}`);
+console.log(`meu nome é ${nome}`);
+console.log(`meu endereço é ${endereco}`);
